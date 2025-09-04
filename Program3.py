f7474b22b55bd905cd1165db1605e5592392cde2
Deepanshudubey2005 <deepanshudubey66@gmail.com>
2025-09-04 11:29:27 +0530

    Max of 3 numbers

---
+++ Program3.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the 1st number :"))
+b=int(input("Enter the 2nd number :"))
+c=int(input("Enter the 3rd number :"))
+if a>b and a>c :
+    print('A is greatest')
+elif b>c  :
+    print("B is greatest")
+else :
+    print("C is greatest ")
