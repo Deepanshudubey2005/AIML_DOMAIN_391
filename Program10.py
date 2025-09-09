6e57c2e0b5ba0568ef4d0e7d49f0d298d6815465
Deepanshudubey2005 <deepanshudubey66@gmail.com>
2025-09-09 22:58:17 +0530

    Table of entered  number

--- Program10.py
+++ Program10.py
@@ -1,6 +1,15 @@
 a=int(input("Enter the number whose table you want to print : "))
 for i in range(1,11):
     s=a*i
-    print(a,"*",i,"=",s)
+    print("{} * {} = {}".format(a,i,s))
+a=int(input("enter the no. whose table u want to print:"))
+d=1   
+i=1 
+while i<11 :
+    d=a*i
     
+    print("{} * {} = {}".format(a,i,d))
+    i=i+1    
+
+
 
